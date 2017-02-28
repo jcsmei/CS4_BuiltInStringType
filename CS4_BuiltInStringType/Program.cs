e4afecf7a8f17aec9733d29ab3d6d98f2a354a7e
jcsme <jcsme@DESKTOP-O6725QL>
2017-02-28 10:56:49 -0500

    Add project files.

---
+++ CS4_BuiltInStringType/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS4_BuiltInStringType
+{
+    class Program
+    {
+        static void Main()
+        {
+            string Name = "\"My name is\n John\n Doe.\n\""; //escape sequences
+            Console.WriteLine(Name);
+
+            string path = @"C:\MyComputer\DeskTop"; //verbatim literal @, prints all chars
+            Console.WriteLine(path);
+
+            Console.ReadLine();
+        }
+    }
+}
